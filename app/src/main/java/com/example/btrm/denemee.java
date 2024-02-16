d80360b7572a02466cc041c98e34a39ad821b10d
kaanrks <160018791+kaanrks@users.noreply.github.com>
2024-02-16 22:34:56 +0300

    son kez dene

--- app/src/main/java/com/example/btrm/denemee.java
+++ app/src/main/java/com/example/btrm/denemee.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class sondeneme extends AppCompatActivity {
+public class denemee extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sondeneme);
+        setContentView(R.layout.activity_denemee);
     }
 }
